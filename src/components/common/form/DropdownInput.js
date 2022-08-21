eff716584cc3bc06ce29f3ae25a1f9fb139c25a0
theNvN <thenvnsahu@gmail.com>
2022-08-21 13:47:00 +0530

    add custom editable & dropdown components

---
+++ src/components/common/form/DropdownInput.js
@@ -0,0 +1,77 @@
+import {
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  FormErrorMessage,
+  VStack,
+  Text,
+} from '@chakra-ui/react';
+import {
+  AutoComplete,
+  AutoCompleteInput,
+  AutoCompleteItem,
+  AutoCompleteList,
+} from '@choc-ui/chakra-autocomplete';
+import { useController } from 'react-hook-form';
+
+const FormDropdownInput = ({
+  name,
+  label,
+  control,
+  placeholder,
+  helperText,
+  defaultValue,
+  isRequired,
+  options = [],
+  _input,
+  ...props
+}) => {
+  const { field, formState } = useController({ name, control, defaultValue });
+  const error = formState?.errors?.[name]?.message;
+
+  const handleSelect = e => {
+    console.log('onSelect', e.item.value);
+    field.onChange(e.item?.value);
+  };
+  const handleChange = e => {
+    console.log('onCHange', e?.target?.value);
+    field.onChange(e.target.value);
+  };
+
+  return (
+    <FormControl pb={2} isRequired={isRequired} isInvalid={!!error} {...props}>
+      <FormLabel>{label}</FormLabel>
+      <AutoComplete
+        onSelectOption={handleSelect}
+        listAllValuesOnFocus={true}
+        openOnFocus={true}
+        value={field.value}
+        rollNavigation
+      >
+        <AutoCompleteInput
+          placeholder={placeholder}
+          value={field.value}
+          onChange={handleChange}
+          onBlur={field.onBlur}
+          {..._input}
+        />
+        <AutoCompleteList>
+          {options.map(opt => (
+            <AutoCompleteItem key={opt.value} value={opt.value} label={opt.label}>
+              <VStack spacing={0} alignItems="start">
+                <Text fontSize="xs" variant="body">
+                  {opt.label}
+                </Text>
+                <Text fontSize="sm">{opt.value}</Text>
+              </VStack>
+            </AutoCompleteItem>
+          ))}
+        </AutoCompleteList>
+      </AutoComplete>
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+      <FormErrorMessage>{error}</FormErrorMessage>
+    </FormControl>
+  );
+};
+
+export default FormDropdownInput;
---
+++ src/components/common/form/EditableInput.js
@@ -0,0 +1,42 @@
+import { HStack, IconButton, Input, Text } from '@chakra-ui/react';
+import { CheckIcon, EditIcon } from 'components/icons';
+import { useState } from 'react';
+
+function EditableInput({ input, value, onSubmit, ...props }) {
+  const [editMode, setEditMode] = useState(false);
+  const [val, setValue] = useState(value);
+
+  const handleSubmit = () => {
+    setEditMode(false);
+    onSubmit && onSubmit(val);
+  };
+
+  return (
+    <HStack w="60%" justify="center" {...props}>
+      {editMode ? (
+        <>
+          <Input
+            size="sm"
+            textAlign="center"
+            value={val}
+            onChange={e => setValue(e.target.value)}
+            {...input}
+          />
+          <IconButton size="sm" icon={<CheckIcon />} variant="ghost" onClick={handleSubmit} />
+        </>
+      ) : (
+        <>
+          <Text textAlign="center">{value}</Text>
+          <IconButton
+            size="sm"
+            icon={<EditIcon />}
+            variant="ghost"
+            onClick={() => setEditMode(true)}
+          />
+        </>
+      )}
+    </HStack>
+  );
+}
+
+export default EditableInput;
