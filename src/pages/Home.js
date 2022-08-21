ca871f12b1950cfba4c2334013dce7ff13422259
theNvN <thenvnsahu@gmail.com>
2022-08-21 13:47:45 +0530

    cleanup

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,55 +1,12 @@
 import { BottomNavLayout } from 'components/layout';
-// import UniversalProfile from 'components/profile/UniversalProfile';
-
-// import { AssetList, AssetTabs } from 'components/digital-asset';
 import { AccountView } from 'components/account';
-// import {
-//   Modal,
-//   ModalBody,
-//   ModalCloseButton,
-//   ModalContent,
-//   ModalFooter,
-//   ModalHeader,
-//   ModalOverlay,
-// } from '@chakra-ui/react';
 
 const Home = () => {
   return (
     <BottomNavLayout>
       <AccountView />
-      {/* <UniversalProfile /> */}
-      {/* <AssetList mx={4} /> */}
-      {/* <AssetTabs /> */}
-      {/* <BasicUsage /> */}
     </BottomNavLayout>
   );
 };
 
-// function BasicUsage() {
-//   // const { isOpen, onOpen, onClose } = useDisclosure();
-//   return (
-//     <>
-//       {/* <Button onClick={onOpen}>Open Modal</Button> */}
-
-//       <Modal isOpen={true} size="sm">
-//         <ModalOverlay />
-//         <ModalContent>
-//           <ModalHeader>Modal Title</ModalHeader>
-//           <ModalCloseButton />
-//           <ModalBody>
-//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam quidem, quidem quidem
-//             quidem quidem quidem quidem quidem quidem
-//           </ModalBody>
-
-//           <ModalFooter>
-//             Close
-//             {/* <Button colorScheme='blue' mr={3} onClick={onClose}>
-//             </Button>
-//             <Button variant='ghost'>Secondary Action</Button> */}
-//           </ModalFooter>
-//         </ModalContent>
-//       </Modal>
-//     </>
-//   );
-// }
 export default Home;
--- src/scripts/keyringController.js
+++ src/scripts/keyringController.js
@@ -14,17 +14,11 @@ const setVault = data => {
   localStorage.setItem(KEY_VAULTS, data);
 };
 
-// let keyringController = null;
-
 const vault = getVault();
-// console.log('kc:vault:', vault);
 const keyringController = new KeyringController({ initState: { vault } });
 keyringController.fullUpdate();
 keyringController.on('update', async (...args) => {
-  console.log('persisted state');
   const state = keyringController.store.getState();
-  // console.log({ args });
-  // console.log(state);
   setVault(state.vault);
 });
 
